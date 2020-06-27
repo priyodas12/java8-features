d72c8849288a5f6e8a79929ccefebfe9a7af8123
Priyobrato Das <priyodas12@gmail.com>
2020-06-27 23:35:54 +0530

    string conditional concat

--- src/io/stream/StringConcatination.java
+++ src/io/stream/StringConcatination.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class StringConcatination {
     public static void main(String[] args) {
         String [] s=new String[]{"avs","hdf","essd","des","awq","fgr","jef","dhg"};
-
+        //conditional adding
         Arrays.asList(s).stream().reduce((s1,s2)->s1+"*"+s2).ifPresent(System.out::println);
     }
 }
