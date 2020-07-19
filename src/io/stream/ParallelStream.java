ed9616f38638f699cf430dfa90e67e93713ef0f8
Priyobrato Das <priyodas12@gmail.com>
2020-07-19 19:03:17 +0530

    parallel stream added

--- src/io/stream/ParallelStream.java
+++ src/io/stream/ParallelStream.java
@@ -2,14 +2,19 @@ package io.stream;
 
 import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class ParallelStream {
 
     public static void main(String[] args) {
-       Integer [] a=new Integer[]{3,4,5,1,5,3,2};
+       //Integer [] a=new Integer[]{3,4,5,1,5,3,2};
        long startTime = System.currentTimeMillis();
-       Arrays.asList(a).stream().filter(i->i%2==0).parallel().forEach(System.out::println);
+       IntStream.range(0,50).filter(i->i%2==0).forEach(System.out::println);
        long endTime = System.currentTimeMillis();
-       System.out.println("time taken "+(endTime-startTime));
+       System.out.println("time taken for stream "+(endTime-startTime));
+       startTime = System.currentTimeMillis();
+       IntStream.range(0,50).parallel().filter(i->i%2==0).forEach(System.out::println);
+       endTime = System.currentTimeMillis();
+       System.out.println("time taken for parallel stream "+(endTime-startTime));
     }
 }
